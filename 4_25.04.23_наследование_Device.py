f47c0770da1d3bfaf048e2283d2bc2f63e37434b
Irina <ga-irina@yandex.ru>
2023-04-08 20:53:06 +0300

    Реализовала в осн.прогламме классы CoffeeMachine и Blender

--- 4_25.04.23_наследование_Device.py
+++ 4_25.04.23_наследование_Device.py
@@ -18,6 +18,45 @@ class Device:
     @property
     def name(self):
         return self.__name
+    @name.setter
+    def name(self, name: str):
+        self.__name = name
+
+    @property
+    def power(self):
+        return self.__power
+    @power.setter
+    def power(self, power: int):
+        self.__power = power
+
+    @property
+    def voltage(self):
+        return self.__voltage
+    @voltage.setter
+    def voltage(self, voltage: int):
+        self.__voltage = voltage
+
+    @property
+    def colour(self):
+        return self.__colour
+    @colour.setter
+    def colour(self, colour: str):
+        self.__colour = colour
+
+    @property
+    def date_manufacture(self):
+        return self.__date_manufacture.values()
+    @date_manufacture.setter
+    def date_manufacture(self, date_manufacture: Dict[str, str]):
+        self.__date_manufacture = date_manufacture.copy()
+
+    def info(self):
+        print(f"Класс: {self.__class__.__name__} \n"
+              f"Наименование изделия: {self.__name} \n"
+              f"Мощность: {self.__power} \n"
+              f"Напряжение: {self.__voltage} \n"
+              f"Цвет: {self.__colour} \n"
+              f"Дата изготовления: {list(self.__date_manufacture.values())}")
 
 
 class CoffeeMachine(Device):
@@ -27,18 +66,64 @@ class CoffeeMachine(Device):
         self.__pressure = pressure
         self.__number_drinks = number_drinks
 
+    @property
+    def pressure(self):
+        return self.__pressure
+    @pressure.setter
+    def pressure(self, pressure: int):
+        self.__pressure = pressure
+
+    @property
+    def number_drinks(self):
+        return self.__number_drinks
+    @number_drinks.setter
+    def number_drinks(self, number_drinks: int):
+        self.__number_drinks = number_drinks
+
+    def info(self):
+        super().info()
+        print(f"Давление: {self.__pressure} \n"
+              f"Количество напитков: {self.__number_drinks}")
+
+
 class Blender(Device):
+
     def __init__(self, name: str, power: int, voltage: int, colour: str, date_manufacture: Dict[str, str],
                  type_blender: str, number_speeds: int = None):
         super().__init__(name, power, voltage, colour, date_manufacture)
         self.__type_blender = type_blender
         self.__number_speeds = number_speeds
 
+    @property
+    def type_blender(self):
+        return self.__type_blender
+
+    @type_blender.setter
+    def type_blender(self, type_blender: str):
+        self.__type_blender = type_blender
+
+    @property
+    def number_speeds(self):
+        return self.__number_speeds
+
+    @number_speeds.setter
+    def number_speeds(self, number_speeds: int):
+        self.__number_speeds = number_speeds
+
+    def info(self):
+        super().info()
+        print(f"Тип блендера: {self.__type_blender} \n"
+              f"Количество скоростей: {self.__number_speeds}")
+
+
 def execute_application():
-    pass
 
+    coffeeMachine = CoffeeMachine("Кофемашина", 2000, 220, "белая", {"число": 12,"месяц": "января", "год": 2022}, 19, 5)
+    coffeeMachine.info()
+    print()
 
-
+    blender = Blender("Блендер", 1500, 220, "черный", {"число": 5,"месяц": "октября", "год": 2021}, "погружной", 15)
+    blender.info()
 
 if __name__ == "__main__":
     execute_application()
