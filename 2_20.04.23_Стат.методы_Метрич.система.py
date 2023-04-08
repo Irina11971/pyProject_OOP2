22421c845fc8773f323b2af431231ec11943d934
Irina <ga-irina@yandex.ru>
2023-04-08 16:49:33 +0300

    Реализовала конвертацию длин из метрической системы в английскую и наоборот.

--- 2_20.04.23_Стат.методы_Метрич.система.py
+++ 2_20.04.23_Стат.методы_Метрич.система.py
@@ -115,11 +115,69 @@ class ConverterLengthMeasure:
         return kilometer / 1.609
 
 
-
 def execute_application():
-    pass
+    print("1 - перевод дюймов в метрическую систему \n"
+          "2 - перевод футов в метрическую систему \n"
+          "3 - перевод ярдов в метрическую систему \n"
+          "4 - перевод миль в метрическую систему \n"
+          "5 - перевод милиметров в английскую систему \n"
+          "6 - перевод сантиметров в английскую систему \n"
+          "7 - перевод метров в английскую систему \n"
+          "8 - перевод километров в английскую систему \n"
+          "9 - выход из конвертора \n")
 
+    while (True):
+        try:
+            N = int(input("Введите вариант действия (цифры 1-9): "))
+            if N == 1:
+                inch = float(input("Введите количество дюймов: "))
+                millimeter = ConverterLengthMeasure.get_inch_in_metricSystem(inch)
+                print(f"{inch} дюйм(а/ов) это {millimeter: 0.2f} милиметра.")
+                continue
+            elif N == 2:
+                foot = float(input("Введите количество футов: "))
+                meter = ConverterLengthMeasure.get_foot_in_metricSystem(foot)
+                print(f"{foot} фут(а/ов) это {meter: 0.2f} метра.")
+                continue
+            elif N == 3:
+                yard = float(input("Введите количество ярдов: "))
+                meter = ConverterLengthMeasure.get_yard_in_metricSystem(yard)
+                print(f"{yard} ярд(а/ов) это {meter: 0.2f} метра.")
+                continue
+            elif N == 4:
+                mile = float(input("Введите количество миль: "))
+                kilometer = ConverterLengthMeasure.get_mile_in_metricSystem(mile)
+                print(f"{mile} миля(ей/и) это {kilometer: 0.2f} километра.")
+                continue
+            elif N == 5:
+                millimeter = float(input("Введите количество милиметров: "))
+                inch = ConverterLengthMeasure.get_millimeter_in_englishSystem(millimeter)
+                print(f"{millimeter} милиметр(а/ов) это {inch: 0.2f} дюйма.")
+                continue
+            elif N == 6:
+                centimetre = float(input("Введите количество сантиметров: "))
+                inch = ConverterLengthMeasure.get_centimetre_in_englishSystem(centimetre)
+                print(f"{centimetre} сантиметр(а/ов) это {inch: 0.2f} дюйма.")
+                continue
+            elif N == 7:
+                metre = float(input("Введите количество метров: "))
+                foot = ConverterLengthMeasure.get_metre_in_englishSystem(metre)
+                print(f"{metre} метр(а/ов) это {foot: 0.2f} ярда.")
+                continue
+            elif N == 8:
+                kilometer = float(input("Введите количество километров: "))
+                mile = ConverterLengthMeasure.get_kilometer_in_englishSystem(kilometer)
+                print(f"{kilometer} километр(а/ов) это {mile: 0.2f} мили.")
+                continue
+            elif N == 9:
+                print("Вы вышли из конвертора единиц длины.")
+                break
+            else:
+                print(f"Некорректно введен вариант действий. Введите цифру от 1 до 9. \n")
+                continue
 
+        except ValueError as e:
+            print("Невозможно привести к числовому значению. Введите данные повторно.")
 
 
 if __name__ == "__main__":
