90b4cb412515fc44446ae2bb677eb7fc4773fbfc
Irina <ga-irina@yandex.ru>
2023-04-09 16:20:41 +0300

    Создала классы Square, Rectangle, Circle и Ellipse

--- main.py
+++ main.py
@@ -1,33 +1,108 @@
-# Объектно-ориентированное программирование
-# Статические методы класса
+from abc import ABC
 
-# Создайте класс Device, который содержит информацию об устройстве.
-# С помощью механизма наследования, реализуйте класс CoffeeMachine
-# (содержит информацию о кофемашине), класс Blender (содержит информацию
-# о блендере).
+# 9.6 Объектно-ориентированное программирование
+# Абстрактные классы
 
-class Device:
-    def __init__(self, passport_number: str, surname: str, name: str, gender: str, birthdate: Dict[str, str],
-                 birthplace: str, date_of_issue: Dict[str, str], patronymic: str = None):
-        self.__passport_number = passport_number
-        self.__surname = surname
-        self.__name = name
-        self.__patronymic = patronymic
-        self.__gender = gender
-        self.__birthdate = birthdate.copy()
-        self.__birthplace = birthplace
-        self.__date_of_issue = date_of_issue.copy()
+# Создайте базовый класс Shape для хранения плоских фигур.
+# Определите производные классы:
+# Square — квадрат, который характеризуется координатами левого
+# верхнего угла и длиной стороны;
+# Rectangle — прямоугольник с заданными координатами верхнего
+# левого угла и размерами;
+# Circle — окружность с заданными координатами цен-тра и радиусом;
+# Ellipse — эллипс с заданными координатами верхнего угла описанного
+# вокруг него прямоугольника со сторонами, параллельными осям координат,
+# и размерами этого прямоугольника.
+# Создайте список фигур. Определите класс, который сохраняет фигуры
+# в файлы, загружает из файла и отобразите информацию о каждой из фигур.
 
 
 
-class International_passport(Passport):
+class Shape:
+    def __init__(self, x: int, y: int):
+        self.__x = x
+        self.__y = y
 
-    def __init__(self, passport_number: str, surname: str, name: str, gender: str, birthdate: Dict[str, str], \
-                 birthplace: str, date_of_issue: Dict[str, str], code_of_issuing_State: str, \
-                 date_of_expiry: Dict[str, str], patronymic: str = None):
-        super().__init__(passport_number, surname, name, gender, birthdate, birthplace, date_of_issue, patronymic)
-        self.__code_of_issuing_State = code_of_issuing_State
-        self.__date_of_expiry = date_of_expiry.copy()
+    @property
+    def x(self):
+        return self.__x
+    @x.setter
+    def x(self, x):
+        self.__x = x
+
+    @property
+    def y(self):
+        return self.__y
+    @y.setter
+    def y(self, y):
+        self.__y = y
+
+
+class Square(Shape):
+    def __init__(self, x: int, y: int, width: int):
+        super().__init__(x, y)
+        self.__width = width
+
+    @property
+    def width(self):
+        return self.__width
+    @width.setter
+    def width(self, width):
+        self.__width = width
+
+class Rectangle(Shape):
+    def __init__(self, x: int, y: int, width: int, height: int):
+        super().__init__(x, y)
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+    @width.setter
+    def width(self, width):
+        self.__width = width
+
+    @property
+    def height(self):
+        return self.__height
+    @height.setter
+    def height(self, height):
+        self.__height = height
+
+class Circle(Shape)
+    def __init__(self, x: int, y: int, radius: int):
+        super().__init__(x, y)
+        self.__radius = radius
+
+    @property
+    def radius(self):
+        return self.__radius
+    @radius.setter
+    def radius(self, radius):
+        self.__radius = radius
+
+class Ellipse(Shape):
+    def __init__(self, x: int, y: int, width: int, height: int):
+        super().__init__(x, y)
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+    @width.setter
+    def width(self, width):
+        self.__width = width
+
+    @property
+    def height(self):
+        return self.__height
+    @height.setter
+    def height(self, height):
+        self.__height = height
+
+
 
 
 
